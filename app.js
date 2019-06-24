6683a530c59ece5c11485f2e631c21ee9364144a
Henrique Gogó <henriquegogo@gmail.com>
2019-06-24 03:16:06 -0300

    First commit - Todo example and module script

---
+++ app.js
@@ -0,0 +1,54 @@
+import Forro from './forro.js';
+const { div, h1, form, input, ul, li, button, b, span } = Forro;
+
+function InputForm(add) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { input_task } = e.target;
+    add(input_task.value);
+    input_task.value = "";
+  };
+
+  return form({ onsubmit: handleSubmit },
+    input({
+      name: 'input_task',
+      placeholder: 'Type a task',
+      style: 'width: 476px',
+      autofocus: true
+    }),
+  );
+}
+
+function Tasks(tasklist) {
+  return ul(
+    ...tasklist.map(text =>
+      li(text)
+    )
+  );
+}
+
+function App(tasklist) {
+  let app, tasks;
+  tasklist = tasklist || [];
+
+  const add = (text) => {
+    tasklist.push(text);
+    app.updateChildren(App(tasklist));
+  }
+
+  const reverse = () => {
+    tasks.replaceWith(tasks=Tasks(tasklist.reverse()));
+  }
+
+  return app = div(
+    h1('Todo List'),
+    InputForm(add, tasklist),
+    tasks=Tasks(tasklist),
+    div(
+      button({ style: 'float: right', onclick: reverse }, 'Reverse'),
+      b('Total: '), span(tasklist.length),
+    ),
+  );
+}
+
+document.body.append(App());
---
+++ forro.js
@@ -0,0 +1,19 @@
+const Forro = {};
+"a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(tag => {
+  Forro[tag] = function(...children) {
+    const element = document.createElement(tag);
+    if (children[0] && children[0].constructor === Object) Object.assign(element, children[0]);
+    children.forEach(child => {
+      child.constructor === String || child.constructor === Number ? element.innerText = child :
+      child.constructor !== Object ? element.appendChild(child) : null;
+    });
+    element.updateChildren = function(update) {
+      [...update.children].forEach((child, i) => {
+        !child.isEqualNode(element.children[i]) && element.replaceChild(child, element.children[i]);
+      });
+      return element;
+    }
+    return element;
+  }
+});
+export default Forro;
