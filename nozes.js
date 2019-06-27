1947001fa32c6d824c1d2354c91b4e678c39597e
Henrique Gogó <henriquegogo@gmail.com>
2019-06-27 00:31:35 -0300

    Refactoring lib code to be more compatible with old browsers - except about 'export' module

--- nozes.js
+++ nozes.js
@@ -1,30 +1,39 @@
-const Nozes = {};
-"a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(tag => {
-  Nozes[tag] = function(...props) {
-    const element = document.createElement(tag);
-    props.forEach(child => {
+"use strict";
+
+var Nozes = {};
+"a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(function(tag) {
+  Nozes[tag] = function() {
+    var props = [].slice.call(arguments);
+    var element = document.createElement(tag);
+    props.forEach(function(child) {
       child.constructor === Object ? Object.assign(element, child) :
       child.constructor === String || child.constructor === Number ? element.innerText = child :
       child.constructor.name.includes('Element') ? element.appendChild(child) : null;
     });
     return element;
-  }
+  };
 });
 export default Nozes;
 
-const container = document.createDocumentFragment();
-
+var container = document.createDocumentFragment();
 export function watch(events, func) {
-  return (...props) => {
-    let element = func(...props);
-    events.split(' ').forEach(name => container.addEventListener(name, () => {
-      const updated = func(...props);
-      if (!element.isEqualNode(updated)) element.replaceWith(element=updated);
-    }));
+  return function() {
+    var props = arguments;
+    var element = func(props);
+    events.split(' ').forEach(function(name) {
+      container.addEventListener(name, function() {
+        var updated = func(props);
+        if (!element.isEqualNode(updated)) {
+          element.parentNode.replaceChild(updated, element);
+          element = updated;
+        }
+      });
+    });
     return element;
   }
 }
-
 export function dispatch(events) {
-  events.split(' ').forEach(name => container.dispatchEvent(new Event(name)));
+  events.split(' ').forEach(function(name) {
+    container.dispatchEvent(new Event(name));
+  });
 }
