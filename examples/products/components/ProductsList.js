0e69ea3baff820ba299abd7d9d2d9255f4af98db
Henrique Gogó <henriquegogo@gmail.com>
2019-07-08 12:59:32 -0300

    Seeding products

--- examples/products/components/ProductsList.js
+++ examples/products/components/ProductsList.js
@@ -1,12 +1,16 @@
 import Nozes from '../../../nozes.js';
-const { ul, li, img, span } = Nozes;
+const { div, a, img, h1, h4, span } = Nozes;
 
 function ProductsList() {
-  return ul({ className: 'products' },
-    ...list.map(item =>
-      li(
-        img(item.img),
-        span(item.label)
+  return div(
+    h1('Novidades'),
+    div({ className: 'products' },
+      ...list.map(item =>
+        a({ href: '#' },
+          img({ src: item.img }),
+          h4(item.title),
+          span(item.description)
+        )
       )
     )
   );
@@ -14,14 +18,18 @@ function ProductsList() {
 
 export default ProductsList;
 
+const batom = 'https://cdn.shopify.com/s/files/1/1495/0402/products/1_be3b6eb3-dbdf-44ca-9c35-c5c4eff53e2e_grande.jpg';
+const sabonete = 'https://jnj-content-lab.brightspotcdn.com/dims4/default/31d7d80/2147483647/strip/true/crop/2000x2000+0+0/resize/360x360!/quality/90/?url=https%3A%2F%2Fjnj-content-lab.brightspotcdn.com%2F5d%2F28%2Ff1bb2b1a493b86050d928f3dcff3%2Fj-babu-golden-new-pump.png';
+const celular = 'https://4.imimg.com/data4/BB/RH/MY-15241145/multimedia-mobile-phone-500x500.jpg';
+
 const list = [
-  { img: '', label: 'Product 1' },
-  { img: '', label: 'Product 2' },
-  { img: '', label: 'Product 3' },
-  { img: '', label: 'Product 4' },
-  { img: '', label: 'Product 5' },
-  { img: '', label: 'Product 6' },
-  { img: '', label: 'Product 7' },
-  { img: '', label: 'Product 8' },
-  { img: '', label: 'Product 9' }
+  { img: batom, title: 'Product 1', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: sabonete, title: 'Product 2', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: celular, title: 'Product 3', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: batom, title: 'Product 4', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: sabonete, title: 'Product 5', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: celular, title: 'Product 6', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: batom, title: 'Product 7', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: sabonete, title: 'Product 8', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
+  { img: celular, title: 'Product 9', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' }
 ];
