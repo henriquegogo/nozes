14da5a4fa1de3f9754b4968f71d48dd6eee92aa6
Henrique Gogó <henriquegogo@gmail.com>
2019-07-14 02:54:42 -0300

    Embedding router into library. Quite simple and just hash route

--- examples/products/App.js
+++ examples/products/App.js
@@ -1,14 +1,13 @@
-import Nozes from '../../nozes.js';
+import Nozes, { router } from '../../nozes.js';
 const { div, footer } = Nozes;
 
-import Router from './Router.js';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 
 function App() {
   return div(
     Header(),
-    Router({
+    router({
       index: Main,
       category: param => div('Category ' + param),
       product: param => div('Product ' + param),
--- nozes.js
+++ nozes.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var Nozes = {};
 "a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(function(tag) {
   Nozes[tag] = function() {
@@ -38,4 +36,12 @@ export function dispatch(events, props) {
   events.split(' ').forEach(function(name) {
     container.dispatchEvent(new CustomEvent(name, { detail: props }));
   });
+}
+
+export function router(routes) {
+  window.onhashchange = dispatch.bind(null, 'route');
+  return watch('route', function() {
+    var route = location.hash.split('/');
+    return route[1] && routes[route[1]] ? routes[route[1]](route[2]) : routes.index(route[2]);
+  })();
 }
