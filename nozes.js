f3d1d3f85449161c40796d331ff3cf6235eb4e59
Henrique Gogó <henriquegogo@gmail.com>
2019-07-19 02:06:50 -0300

    Just some var name refactor and scope

--- nozes.js
+++ nozes.js
@@ -2,11 +2,11 @@ var Nozes = {};
 "a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(function(tag) {
   Nozes[tag] = function() {
     var element = document.createElement(tag);
-    [].slice.call(arguments).forEach(function(child) {
-      if (child != null) {
-        child.constructor === Object ? Object.assign(element, child) :
-        child.constructor === String || child.constructor === Number ? element.innerText = child :
-        child.constructor.name.includes('Element') ? element.appendChild(child) : null;
+    [].slice.call(arguments).forEach(function(arg) {
+      if (arg != null) {
+        arg.constructor === Object ? Object.assign(element, arg) :
+        arg.constructor === String || arg.constructor === Number ? element.innerText = arg :
+        arg.constructor.name.includes('Element') && element.appendChild(arg);
       }
     });
     return element;
@@ -14,13 +14,13 @@ var Nozes = {};
 });
 export default Nozes;
 
-var container = document.createDocumentFragment();
 export function watch(events, func) {
+  watch.listener = watch.listener || document.createDocumentFragment();
   return function() {
     var props = [].slice.call(arguments);
     var element = func.apply(null, props);
     events.split(' ').forEach(function(name) {
-      container.addEventListener(name, function(e) {
+      watch.listener.addEventListener(name, function(e) {
         props[0] = props[0] && props[0].constructor === Object ? Object.assign(props[0], e.detail) : e.detail;
         var updated = func.apply(null, props);
         if (element != null && element.parentNode && (!element.isEqualNode(updated) || e.detail != null)) {
@@ -34,7 +34,7 @@ export function watch(events, func) {
 }
 export function dispatch(events, props) {
   events.split(' ').forEach(function(name) {
-    container.dispatchEvent(new CustomEvent(name, { detail: props }));
+    watch.listener.dispatchEvent(new CustomEvent(name, { detail: props }));
   });
 }
 
