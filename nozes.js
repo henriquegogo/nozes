f6925fd2028777b88a09bf09959b2f53eaac033c
Henrique Gogó <henriquegogo@gmail.com>
2019-07-19 11:24:42 -0300

    Using reduce instead of declare an object and set props

--- nozes.js
+++ nozes.js
@@ -1,6 +1,5 @@
-var Nozes = {};
-"a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").forEach(function(tag) {
-  Nozes[tag] = function() {
+export default "a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr".split(" ").reduce(function(result, tag) {
+  result[tag] = function() {
     var element = document.createElement(tag);
     [].slice.call(arguments).forEach(function(arg) {
       if (arg != null) {
@@ -11,9 +10,8 @@ var Nozes = {};
     });
     return element;
   };
-});
-export default Nozes;
-
+  return result;
+}, {});
 export function watch(events, func) {
   watch.listeners = watch.listeners || [];
   return function() {
