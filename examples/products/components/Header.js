457703469885028295b9bc2d1ffe03ad05fe8eb0
Henrique Gogó <henriquegogo@gmail.com>
2019-07-20 01:53:53 -0300

    Home link on app title

--- examples/products/components/Header.js
+++ examples/products/components/Header.js
@@ -1,10 +1,10 @@
 import Elements from '../../../nozes.js';
-const { header, section, div, h1, input, i, label, span } = Elements;
+const { header, section, div, h1, input, i, label, span, a } = Elements;
 
 function Header() {
   return header(
     section(
-      h1('SHOPPS'),
+      a({ href: '#' }, h1('SHOPPS')),
       label(
         input({ type: 'text', placeholder: 'Estou procurando...' }),
         i({ className: 'icon search-icon' }, 'search')
