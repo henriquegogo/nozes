ed8c36b07a6eb147b737f72f302dd610778fc3c1
Henrique Gogó <henriquegogo@gmail.com>
2019-07-26 01:15:34 -0300

    Moving jsx to folders

---
+++ examples/jsx/src/App.js
@@ -0,0 +1,13 @@
+import Elements from '../../../nozes.js';
+import Message from './Message.js';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+      <Message>Lorem iMessagesum sicut dixit</Message>
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/jsx/src/Message.js
@@ -0,0 +1,7 @@
+import Elements from '../../../nozes.js';
+
+function Message(msg) {
+  return <p>{msg}</p>;
+}
+
+export default Message;
