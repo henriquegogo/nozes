470b60a80f9d86f067e80de768d51a3b1ee1752f
Henrique Gogó <henriquegogo@gmail.com>
2019-07-28 15:34:05 -0300

    Removing id of input and getting reference from event

--- examples/todo/InputForm.js
+++ examples/todo/InputForm.js
@@ -6,11 +6,10 @@ setTimeout(() => dispatch('add', ['Async first item', 'Async second item']), 100
 function InputForm() {
   function handleChange(e) {
     dispatch('add', e.target.value);
-    window.inputTask.value = '';
+    e.target.value = '';
   }
 
   return input({
-    id: 'inputTask',
     placeholder: 'Type a task',
     style: 'width: 476px',
     autofocus: true,
