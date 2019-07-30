1e0482cb83d48f4d64ede9443d3af44761ddf7da
Henrique Gogó <henriquegogo@gmail.com>
2019-07-30 02:09:08 -0300

    Getting categories list and products from api request

--- examples/products/components/Categories.js
+++ examples/products/components/Categories.js
@@ -1,7 +1,13 @@
-import Elements from '../../../nozes.js';
+import Elements, { connect, dispatch } from '../../../nozes.js';
 const { div, img, a } = Elements;
 
-function Categories() {
+function Categories(list = []) {
+  if (!this) {
+    fetch('./api/categories')
+      .then(res => res.json())
+      .then(res => dispatch('categories', res));
+  }
+
   return div({ className: 'categories' },
     ...list.map(item =>
       a({ href: '#/category/' + item.id },
@@ -12,27 +18,4 @@ function Categories() {
   );
 }
 
-export default Categories;
-
-const celulares = 'https://static.carrefour.com.br/medias/sys_master/images/images/h11/h42/h00/h00/11652553474078.jpg';
-const brinquedos = 'https://ae01.alicdn.com/kf/HTB1zxM9QFXXXXbDaXXXq6xXFXXXl/Brinquedos-do-beb-Musical-Instruments-17-cm-Batendo-Brinquedos-Bebe-Infantil-Pato-Amarelo-Brinquedo-Eletr-nico.jpg';
-const informatica = 'https://www.pontofrio-imagens.com.br/Informatica/Computadores/14449914/1065518711/computador-imac-core-i3-4gb-de-memoria-hd-1tb-215-quot-led-mc509bz-a-apple-computador-apple-imac-ci3-4-1t-215-quot-mc509bz-a-14449914.jpg'
-const som = 'https://www.casasbahia-imagens.com.br/audio/Caixa-de-Som-Portatil/12531579/895071478/caixa-de-som-portatil-jbl-boombox-com-bluetooth-connect-a-prova-dagua-preta-12531579.jpg';
-const eletroportateis = 'https://http2.mlstatic.com/eletroportateis-liquidificador-10-veloc-pulsar-mondial-110v-D_NQ_NP_943217-MLB28982135797_122018-F.jpg';
-const decoracao = 'https://www.e-cadeiras.com.br/ccstore/v1/images/?source=/file/v1253837277991717647/products/cadeira%20eames%20preta%20-%20deeapr-1263-1.jpg&height=475&width=475';
-const moda = 'https://http2.mlstatic.com/vestido-xadrez-roupas-femininas-pronta-entrega-D_NQ_NP_697104-MLB27236349465_042018-Q.jpg';
-const livros = 'https://openbookphilly.com/wp-content/uploads/2016/11/bookstack.png';
-const papelaria = 'https://res-2.cloudinary.com/gaveteiro/image/upload/c_fit,h_1000,w_1000/v1432168667/mqosegda7m4oywj4m6ag.jpg';
-
-const list = [
-  { id: 'celulares', img: celulares, label: 'Celulares e Telefones' },
-  { id: 'brinquedos', img: brinquedos, label: 'Brinquedos' },
-  { id: 'informatica', img: informatica, label: 'Informática' },
-  { id: 'som', img: som, label: 'Som e Áudio' },
-  { id: 'eletroportateis', img: eletroportateis, label: 'Eletroportáteis' },
-  { id: 'decoracao', img: decoracao, label: 'Decoração' },
-  { id: 'moda', img: moda, label: 'Moda e Beleza' },
-  { id: 'livros', img: livros, label: 'Jogos, Livros e Filmes' },
-  { id: 'papelaria', img: papelaria, label: 'Papelaria e Escritório' }
-];
-
+export default connect('categories', Categories);
--- examples/products/components/ProductsList.js
+++ examples/products/components/ProductsList.js
@@ -1,7 +1,13 @@
-import Elements from '../../../nozes.js';
+import Elements, { connect, dispatch } from '../../../nozes.js';
 const { div, a, img, h1, h4 } = Elements;
 
-function ProductsList() {
+function ProductsList(list = []) {
+  if (!this) {
+    fetch('./api/products')
+      .then(res => res.json())
+      .then(res => dispatch('products', res));
+  }
+
   return div(
     h1('Novidades'),
     div({ className: 'products' },
@@ -16,20 +22,4 @@ function ProductsList() {
   );
 }
 
-export default ProductsList;
-
-const batom = 'https://cdn.shopify.com/s/files/1/1495/0402/products/1_be3b6eb3-dbdf-44ca-9c35-c5c4eff53e2e_grande.jpg';
-const sabonete = 'https://jnj-content-lab.brightspotcdn.com/dims4/default/31d7d80/2147483647/strip/true/crop/2000x2000+0+0/resize/360x360!/quality/90/?url=https%3A%2F%2Fjnj-content-lab.brightspotcdn.com%2F5d%2F28%2Ff1bb2b1a493b86050d928f3dcff3%2Fj-babu-golden-new-pump.png';
-const celular = 'https://4.imimg.com/data4/BB/RH/MY-15241145/multimedia-mobile-phone-500x500.jpg';
-
-const list = [
-  { img: batom, title: 'Product 1', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: sabonete, title: 'Product 2', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: celular, title: 'Product 3', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: batom, title: 'Product 4', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: sabonete, title: 'Product 5', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: celular, title: 'Product 6', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: batom, title: 'Product 7', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: sabonete, title: 'Product 8', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' },
-  { img: celular, title: 'Product 9', description: 'Lorem ipsum sicut dixit dolor lamen ourives tchan' }
-];
+export default connect('products', ProductsList);
