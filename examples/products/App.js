4bb212467b4b5a6b9ea5d4fcc2574559ea64f84a
Henrique Soares <henrique.vasconcelos@hp.com>
2019-07-31 10:37:04 -0300

    Connected elements in router needs to be inside a container not connected

--- examples/products/App.js
+++ examples/products/App.js
@@ -1,5 +1,5 @@
 import Elements, { router } from '../../nozes.js';
-const { div, main, section, footer } = Elements;
+const { div, main, footer } = Elements;
 
 import Header from './components/Header.js';
 import Main from './components/Main.js';
@@ -12,7 +12,7 @@ function App() {
       router({
         index: Main,
         category: param => div('Category ' + param),
-        product: param => Product({ id: param }),
+        product: param => div(Product({ id: param }))
       })
     ),
     footer()
