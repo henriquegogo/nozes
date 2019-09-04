4e95b8618a538d792cb9d64fe5bd59d931091041
Henrique Soares <henrique.vasconcelos@hp.com>
2019-09-04 18:25:43 -0300

    Adding connect custom component, appending function name in multiple watch functions

--- nozes.js
+++ nozes.js
@@ -1,4 +1,4 @@
-var Elements = 'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr'.split(' ').reduce(function(result, tag) {
+var Elements = 'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr n-connect'.split(' ').reduce(function(result, tag) {
   result[tag] = function() {
     var element = document.createElement(tag);
     [].slice.call(arguments).filter(function(arg) { return arg != null }).forEach(function(arg) {
@@ -18,11 +18,11 @@ var listeners = [];
 function watch(events, func, key) {
   var events_list = Array.isArray(events) ? events : events.split(' ');
   events_list.forEach(function(name) {
-    var found_event = listeners.find(function(listener) { return key !== undefined && listener.key === key });
+    var found_event = listeners.find(function(listener) { return key !== undefined && listener.key === key.concat('_' + name) });
     if (found_event) {
       Object.assign(found_event, { event: name, action: func });
     } else {
-      listeners.push({ event: name, action: func, key: key });
+      listeners.push({ event: name, action: func, key: key && key.concat('_' + name) });
     }
   });
 }
@@ -39,21 +39,21 @@ function dispatch(event, msg) {
 function connect(events, func) {
   return function() {
     var props = [].slice.call(arguments);
-    var element = func.apply(document.createDocumentFragment(), props);
+    var element = func.apply({ isConnected: false }, props);
     watch(events, function() {
-      var updated = func.apply(element, props);
+      var updated = func.apply({ isConnected: true }, props);
       if (element != null && element.parentNode && !element.isEqualNode(updated)) {
         element.parentNode.replaceChild(updated, element);
         element = updated;
       }
     }, func.name ? 'connect_' + func.name : undefined);
-    return element;
+    return Elements['n-connect']({ title: func.name }, element);
   }
 }
 
 function router(routes) {
   window.onhashchange = dispatch.bind(undefined, 'hashchange');
-  return connect('hashchange', function() {
+  return connect('hashchange', function hashchange() {
     var path = window.location.hash.split('/');
     return path[1] && routes[path[1]] ? routes[path[1]].apply(undefined, path.slice(2)) : routes.index.apply(undefined, path.slice(2));
   })();
