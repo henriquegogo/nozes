907ea4b62fbf9fbec443fe1414707afcc983aaf5
Henrique Gogó <henriquegogo@gmail.com>
2019-09-19 03:49:23 -0300

    Setting global as closure variable

--- nozes.js
+++ nozes.js
@@ -1,5 +1,5 @@
-(function() {
-  var store = {}, listeners = [], Nozes = 'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr n-connect'.split(' ').reduce(function(result, tag) {
+(function(global) {
+  var store = {}, listeners = [], Nozes = global.Nozes = 'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr n-connect'.split(' ').reduce(function(result, tag) {
     result[tag] = function() {
       var element = document.createElement(tag);
       [].slice.call(arguments).filter(function(arg) { return arg != null }).forEach(function(arg) {
@@ -50,5 +50,4 @@
       return path[1] && routes[path[1]] ? routes[path[1]].apply(undefined, path.slice(2)) : routes.index.apply(undefined, path.slice(2));
     })();
   }
-  typeof module !== "undefined" ? module.exports = Nozes : window.Nozes = Nozes;
-})();
+})(typeof module !== "undefined" ? module.exports : window);
