3009e8279c7f2233bd3d6788018e556642eaf4fe
Henrique Gogó <henriquegogo@gmail.com>
2019-09-19 21:41:27 -0300

    Reorganizing code and making createElement support JSX native

--- examples/jsx/src/index.js
+++ examples/jsx/src/index.js
@@ -1,4 +1,4 @@
-import '../../../vendor/jsx.js';
+import '../../../nozes.js';
 import App from './App.js';
 
 document.body.appendChild(App());
--- nozes.js
+++ nozes.js
@@ -1,23 +1,28 @@
 (function(global) {
   var store = {}, listeners = [], Nozes = global.Nozes = 'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr n-connect'.split(' ').reduce(function(result, tag) {
-    result[tag] = function() {
-      var element = document.createElement(tag);
-      [].slice.call(arguments).filter(function(arg) { return arg != null }).forEach(function(arg) {
-        arg.constructor === Object ? Object.assign(element, arg) :
-        arg.constructor === Function ? arg(element) :
-        arg.constructor === String || arg.constructor === Number ? element.appendChild(document.createTextNode(arg)) :
-        arg.constructor.name.includes('Element') && element.appendChild(arg);
-      });
-      return element;
-    };
+    result[tag] = createElement.bind(undefined, tag);
     return result;
-  }, { watch: watch, dispatch: dispatch, connect: connect, router: router });
+  }, { createElement: createElement, watch: watch, dispatch: dispatch, connect: connect, router: router });
+
+  function createElement(tag) {
+    if (tag.constructor === Function) return tag.apply(undefined, [].slice.call(arguments).slice(1));
+    var element = document.createElement(tag);
+    [].slice.call(arguments).slice(1).forEach(function(arg) {
+      arg === null || arg.constructor === Object ? Object.assign(element, arg) :
+      arg.constructor === Function ? arg(element) :
+      arg.constructor === String || arg.constructor === Number ? element.appendChild(document.createTextNode(arg)) :
+      arg.constructor.name.includes('Element') && element.appendChild(arg);
+    });
+    return element;
+  };
+  
   function watch(event, func, group) {
     var found_event = listeners.find(function(listener) {
       return group !== undefined && listener.group === group && listener.event === event;
     });
     found_event ? Object.assign(found_event, { action: func }) : listeners.push({ event: event, action: func, group: group });
   }
+  
   function dispatch(event, msg) {
     event.constructor === String && (msg = store[event] = msg.constructor === Object ? Object.assign({}, store[event], msg) : msg);
     listeners.forEach(function(listener) {
@@ -26,6 +31,7 @@
       }
     });
   }
+  
   function connect(events, func) {
     events.constructor === Function ? (func = events, events = []) : !Array.isArray(events) && (events = [events]);
     return function(props) {
@@ -43,6 +49,7 @@
       return Nozes['n-connect']({ title: func.name }, element);
     }
   }
+
   function router(routes) {
     window.onhashchange = dispatch.bind(undefined, { name: 'hashchange' });
     return connect(function hashchange() {
