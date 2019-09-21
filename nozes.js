4dfaa9ba2ede8e2aab3c87b019b555b5c65bc315
Henrique Gogó <henriquegogo@gmail.com>
2019-09-21 18:22:17 -0300

    Using span instead of custom element and title for connected containers

--- nozes.js
+++ nozes.js
@@ -1,7 +1,7 @@
 (function(global) { global.Nozes = function() {
   var store = {}, listeners = [];
   
-  'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr n-connect'.split(' ').forEach(function(tag) {
+  'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr'.split(' ').forEach(function(tag) {
     createElement[tag] = createElement.bind(undefined, tag);
   });
 
@@ -47,7 +47,7 @@
         }, func.name);
       });
       element = func.call({ isConnected: false }, props = Object.assign({}, props, store));
-      return createElement['n-connect']({ title: func.name }, element);
+      return createElement('span', element, function(span) { span.setAttribute('data-connect', func.name) });
     }
   }
 
