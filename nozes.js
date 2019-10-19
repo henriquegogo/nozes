45d264298fdb3ab5e21e83b36b82f873abb496fa
Henrique Gogó <henriquegogo@gmail.com>
2019-10-19 03:44:10 -0300

    Getting tags from official w3c index. Removing obsolete tags

--- nozes.js
+++ nozes.js
@@ -1,7 +1,7 @@
 (function(global) { global.Nozes = function() {
   var store = {}, listeners = [];
-  
-  'a abbr address area article aside b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param pre progress q rb rp rt rtc ruby s samp script section select small source span strong style sub summary sup table tbody td template textarea tfoot th thead time title tr track u ul video wbr'.split(' ').forEach(function(tag) {
+
+  'a abbr address area article aside audio b base bb bdo blockquote body br button canvas caption cite code col colgroup command datagrid datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hr html i iframe img input ins kbd label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby samp script section select small source span strong style sub sup table tbody td textarea tfoot th thead time title tr ul var video'.split(' ').forEach(function(tag) {
     createElement[tag] = createElement.bind(undefined, tag);
   });
 
