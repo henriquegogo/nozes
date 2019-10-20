73dc45b1cc7aff8979abfff0bbb80a00654b5898
Henrique Gogó <henriquegogo@gmail.com>
2019-10-20 00:23:46 -0300

    Updating to all official html tags from w3c specs

--- nozes.js
+++ nozes.js
@@ -1,7 +1,7 @@
 (function(global) { global.Nozes = function() {
   var store = {}, listeners = [];
 
-  'a abbr address area article aside audio b base bb bdo blockquote body br button canvas caption cite code col colgroup command datagrid datalist dd del details dfn dialog div dl dt em embed fieldset figure footer form h1 h2 h3 h4 h5 h6 head header hr html i iframe img input ins kbd label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby samp script section select small source span strong style sub sup table tbody td textarea tfoot th thead time title tr ul var video'.split(' ').forEach(function(tag) {
+  'a abbr address area article aside audio b base bdi bdo blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd label legend li link main map mark math menu meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select slot small source span strong style sub summary sup svg table tbody td template textarea tfoot th thead time title tr track u ul var video wbr'.split(' ').forEach(function(tag) {
     createElement[tag] = createElement.bind(undefined, tag);
   });
 
