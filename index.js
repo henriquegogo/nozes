03dc335f44a232905044b683c6714822ffe4c514
Henrique Gogó <henriquegogo@gmail.com>
2020-07-13 00:03:39 -0300

    Exporting styleClass in ES6 version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './dist/nozes.min.js';
 const nozes = new window.Nozes();
 export const createElement = nozes.createElement;
+export const styleClass = nozes.styleClass;
 export const watch = nozes.watch;
 export const dispatch = nozes.dispatch;
 export const connect = nozes.connect;
