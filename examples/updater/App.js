04a9dae7a45877c6b170af26149dcdb19cd860db
Henrique Gogó <henriquegogo@gmail.com>
2020-11-11 20:42:14 -0300

    Improving updater example

--- examples/updater/App.js
+++ examples/updater/App.js
@@ -1,24 +1,24 @@
 // App.js
 const { createElement, watch, dispatch, update } = new window.Nozes();
-const { div, h1, p } = createElement;
-
-window.watch = watch;
-window.dispatch = dispatch;
+const { div, h1, p, button } = createElement;
 
 function App(props) {
   const { title, desc } = props;
+  const setTitle = title => dispatch(App, { title });
+
+  function setState(state) {
+    update(App, props, state);
+  }
 
   if (this == window) {
-    watch('title', newTitle => update(App, props, { title: newTitle }));
-
-    setTimeout(() => {
-      dispatch('title', Date.now());
-    }, 1000);
+    watch(App, setState);
+    setTimeout(() => setTitle('New title'), 1000);
   }
 
   return props.element = div(
     h1(title),
-    p(desc)
+    p(desc),
+    button('Update', { onclick: () => setState({ title: 'Time is: ' + Date.now()}) })
   );
 }
 
