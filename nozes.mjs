218ff4fd7e84f2b0a3574c70bb4fb75065d0c7a3
Henrique Gogó <henriquegogo@gmail.com>
2021-04-01 01:58:03 -0300

    Simplifying the module

--- nozes.mjs
+++ nozes.mjs
@@ -1,8 +1,3 @@
 import './nozes.js';
-const nozes = new window.Nozes();
-export const createElement = nozes.createElement;
-export const styleClass = nozes.styleClass;
-export const watch = nozes.watch;
-export const dispatch = nozes.dispatch;
-export const connect = nozes.connect;
-export const router = nozes.router;
+const { createElement, styleClass, watch, dispatch, connect, router } = new window.Nozes();
+export { createElement, styleClass, watch, dispatch, connect, router }
